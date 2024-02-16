a25f00b9f21016aef3941585b0fa4b0295412756
mdfaizan973 <faizan.md9735@gmail.com>
2024-02-16 21:45:41 +0530

    craeted a button to the menbership

--- questlabs/src/Components/MamberShip.jsx
+++ questlabs/src/Components/MamberShip.jsx
@@ -1,3 +1,21 @@
+import styled from "styled-components";
 export default function MamberShip() {
-  return <div>MamberShip</div>;
+  return (
+    <DIV>
+      <button>Membership</button>
+    </DIV>
+  );
 }
+const DIV = styled.div`
+  button {
+    padding: 10px 20px;
+    font-size: 16px;
+    font-weight: bold;
+    color: #fff;
+    background-color: #6853f2;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+  }
+`;
